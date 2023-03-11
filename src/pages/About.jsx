1448d93e046ce2ef6c0ef766be8d6172342b7743
ragedestiny <praxair007@gmail.com>
2023-03-11 13:55:50 -0800

    updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,11 +12,11 @@ const About = () => {
           <h1 className="display-4 fw-bold">Welcome to Show ME!!!</h1>
           <p>
             Have you ever heard someone, perhaps your teacher, remark that your
-            writing is boring? How can you do that? Well, Show ME can help you
-            do just that! Instead of writing dull elementary sentences, learn
-            and practice writing vivid and exciting sentences. Don't tell, but
-            show instead! Paint a picture in the readers' minds with mere words.
-            Start today!{" "}
+            writing is boring? How can you write more interestingly? Well, Show
+            ME can help you do just that! Instead of writing dull elementary
+            sentences, learn and practice writing vivid and exciting sentences.
+            Don't tell, but show instead! Paint a picture in the readers' minds
+            with mere words. Start today!{" "}
           </p>
           <a
             onClick={() => navigate("/MyPage")}
